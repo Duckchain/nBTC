693f771a88dd2885f22eb3d12cdc886b381be5e7
Umoh Samuel Godwin <youngking074@gmail.com>
2023-10-07 03:09:37 +0100

    setup for animation

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import imgref from "./imgref";
+import Link from "next/link";
 
 const navElements = [
   imgref.githublogo,
@@ -12,11 +13,13 @@ const navElements = [
 function Footer() {
   return (
     <footer className=" px-[5%] ipad:px-[18px] py-[2.5%] ipad:py-[35.72px] flex flex-col tablet:flex-row items-center justify-center tablet:justify-between w-full pt-[1rem] tablet:pt-[2.5%] gap-[2rem] tablet:gap-0 ">
-      <Image
-        src={imgref.nbtcbnw}
-        alt="nBTC logo"
-        className=" tablet:w-[132px] tablet:h-[32.56px] w-[66px] h-[16.26]"
-      />
+      <Link href="/">
+        <Image
+          src={imgref.nbtcbnw}
+          alt="nBTC logo"
+          className=" tablet:w-[132px] tablet:h-[32.56px] w-[66px] h-[16.26]"
+        />
+      </Link>
 
       <div className=" flex text-white gap-[63px]">
         {navElements.map((navElement, index) => (
--- src/components/purpleishButton.tsx
+++ src/components/purpleishButton.tsx
@@ -6,7 +6,7 @@ type PurpleishButtonProps = {
 
 function PurpleishButton({ children }: PurpleishButtonProps) {
   return (
-    <button className="tablet:w-fit w-full font-normal text-base text-white px-[25px] py-[8px] bg-gradient-to-b from-[#601E75] to-[#2A0536] whitespace-nowrap">
+    <button className="tablet:w-fit w-full font-normal text-base text-white px-[25px] py-[8px] bg-gradient-to-b from-[#601E75] to-[#2A0536] whitespace-nowrap hover:text-[#601E75] hover:bg-gradient-to-b hover:from-transparent hover:to-white hover:border hover:border-[#601E75] transition duration-300 ease-in-out">
       {children}
     </button>
   );
