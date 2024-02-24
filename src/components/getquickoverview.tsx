475aaad16859b7d0cf7f529bd0217b9a7b94ad4d
Umoh Samuel Godwin <umohsg@gmail.com>
2024-02-24 16:38:53 +0100

    Cybria

--- src/components/getquickoverview.tsx
+++ src/components/getquickoverview.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import PurpleishButton2 from "./purpleishButton2";
-import { type } from "os";
 
 type GetQuickOVProps = {
   ShowIndi?: boolean;
@@ -9,40 +8,30 @@ type GetQuickOVProps = {
 };
 
 function GetQuickOV({ ShowIndi, ShowBiz, ShowDev }: GetQuickOVProps) {
+  const sections = [
+    { show: ShowIndi, line1: "Individuals", where: "/why/individual" },
+    { show: ShowBiz, line1: "Businesses", where: "/why/business" },
+    { show: ShowDev, line1: "Developers", where: "/why/developers" },
+  ];
+
   return (
-    <section className=" mt-[10vh] bg-[#1E1E1E] text-white text-center py-[58px] flex flex-col justify-center items-center gap-[52px] px-[2.5%]">
-      <h1 className=" font-bold text-3xl">Get a quick overview for</h1>
-      {/* <div className=" tablet:flex justify-between w-full max-w-full"> */}
-      {/* <GetQuickOV ShowDev ShowIndi /> */}
-
-      <div className=" flex flex-col tablet:flex-row items-center tablet:items-baseline justify-between gap-[58px] w-full max-w-full">
-        <div className={` ${ShowIndi ? "block" : "hidden"} w-full`}>
-          <PurpleishButton2
-            line1Text="Individuals"
-            line2Text="Learn more"
-            where="/why/individual"
-            show={ShowIndi ? true : false}
-          />
-        </div>
-
-        <div className={` ${ShowBiz ? "block" : "hidden"} w-full`}>
-          <PurpleishButton2
-            line1Text="Businesses"
-            line2Text="Learn more"
-            where="/why/business"
-            show={ShowBiz ? true : false}
-          />
-        </div>
-        <div className={` ${ShowDev ? "block" : "hidden"} w-full`}>
-          <PurpleishButton2
-            line1Text="Developers"
-            line2Text="Learn more"
-            where="/why/developers"
-            show={ShowDev ? true : false}
-          />
-        </div>
+    <section className="mt-[10vh] bg-[#1E1E1E] text-white text-center py-[58px] flex flex-col justify-center items-center gap-[52px] px-[2.5%]">
+      <h1 className="font-bold text-3xl">Get a quick overview for</h1>
+      <div className="flex flex-col tablet:flex-row items-center tablet:items-baseline justify-between gap-[58px] w-full max-w-full">
+        {sections.map(
+          (section, index) =>
+            section.show && (
+              <div key={index} className="w-full">
+                <PurpleishButton2
+                  line1Text={section.line1}
+                  line2Text="Learn more"
+                  where={section.where}
+                  show={section.show}
+                />
+              </div>
+            )
+        )}
       </div>
-      {/* </div> */}
     </section>
   );
 }
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,15 +1,20 @@
 import Footer from "@/components/footer";
-import Navbar from "@/components/navbar";
 import React from "react";
+import { CybriaNavBanner } from "./CybriaChain";
+import { Navbar } from "./Navbar";
 
 type LayoutProps = {
   children: React.ReactNode;
 };
 
+// const PurpleToBlackGradient =
+//   "text-transparent bg-clip-text bg-gradient-to-b from-[#5C1D70] to-[#2F073C]";
+
 const Layout = ({ children }: LayoutProps) => {
   return (
     <div id="mylayout" className="w-full h-full">
       <Navbar />
+      <CybriaNavBanner />
       <main>{children}</main>
       <Footer />
     </div>
